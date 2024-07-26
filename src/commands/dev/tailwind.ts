a75b2a138cf4f826785bb8c8b6d7c14700c11ee3
Ribbit <echo07200503@gmail.com>
2024-07-26 17:40:42 +0900

    🐛 出力先が存在しない場合は作成

--- src/commands/dev/tailwind.ts
+++ src/commands/dev/tailwind.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import chalk from 'chalk';
 import { getTailwindConfigFromK2Config, watchTailwindCSS } from '../../lib/tailwind.js';
+import fs from 'fs-extra';
 
 export const watchCss = async (params: { k2Config: K2.Config; outdir: string }) => {
   const { k2Config, outdir } = params;
@@ -12,6 +13,11 @@ export const watchCss = async (params: { k2Config: K2.Config; outdir: string }) 
   const tailwindConfig = await getTailwindConfigFromK2Config(k2Config.tailwind);
 
   const input = path.resolve(k2Config.tailwind.css);
+  const output = path.join(outdir, 'tailwind.css');
+
+  if (!(await fs.pathExists(output))) {
+    await fs.outputFile(output, '');
+  }
 
   return watchTailwindCSS({
     input,
