1577efbf3c19dab57032e1a1d896d0ada64d7b6a
Ribbit <echo07200503@gmail.com>
2024-08-01 11:53:30 +0900

    🐛 linuxでの実行エラーを修正

--- src/lib/import.ts
+++ src/lib/import.ts
@@ -1,5 +1,6 @@
 import { pathToFileURL } from 'url';
 import { CONFIG_FILE_NAME, PLUGIN_CONFIG_FILE_NAME } from './constants.js';
+import path from 'path';
 
 export const esmImport = (path: string) => {
   if (process.platform === 'win32') {
@@ -10,9 +11,9 @@ export const esmImport = (path: string) => {
 };
 
 export const importK2Config = async (configFileName?: string): Promise<K2.Config> => {
-  return (await esmImport(configFileName ?? CONFIG_FILE_NAME)).default;
+  return (await esmImport(path.resolve(configFileName ?? CONFIG_FILE_NAME))).default;
 };
 
 export const importPluginConfig = async (configFileName?: string): Promise<Plugin.Meta.Config> => {
-  return (await esmImport(configFileName ?? PLUGIN_CONFIG_FILE_NAME)).default;
+  return (await esmImport(path.resolve(configFileName ?? PLUGIN_CONFIG_FILE_NAME))).default;
 };
