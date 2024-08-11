eb1ec22b36416d2471e9a3143feee450ad17bc37
Ribbit <echo07200503@gmail.com>
2024-08-11 11:13:47 +0900

    👔 引数追加、フォルダが存在しない場合は作成

--- src/lib/plugin-contents.ts
+++ src/lib/plugin-contents.ts
@@ -3,12 +3,19 @@ import path from 'path';
 import { PLUGIN_CONTENTS_DIRECTORY } from './constants.js';
 import htmlMinifier from 'html-minifier';
 
-export const copyPluginContents = async () => {
-  await fs.copySync(path.join('src', 'contents'), path.join(PLUGIN_CONTENTS_DIRECTORY), {
+export const copyPluginContents = async (
+  params: { inputDir?: string; outputDir?: string } = {}
+) => {
+  const { inputDir = path.join('src', 'contents'), outputDir = PLUGIN_CONTENTS_DIRECTORY } = params;
+  if (!fs.existsSync(inputDir)) {
+    await fs.mkdir(inputDir, { recursive: true });
+  }
+
+  await fs.copy(inputDir, path.join(outputDir), {
     overwrite: true,
   });
 
-  const html = await fs.readFile(path.join(PLUGIN_CONTENTS_DIRECTORY, 'config.html'), 'utf8');
+  const html = await fs.readFile(path.join(outputDir, 'config.html'), 'utf8');
 
   const minified = htmlMinifier.minify(html, {
     minifyCSS: true,
@@ -21,5 +28,5 @@ export const copyPluginContents = async () => {
     useShortDoctype: true,
   });
 
-  await fs.writeFile(path.join(PLUGIN_CONTENTS_DIRECTORY, 'config.html'), minified);
+  await fs.writeFile(path.join(outputDir, 'config.html'), minified);
 };
