5b919bbc98ebe41903b14931db08660fabe2c025
Ribbit <echo07200503@gmail.com>
2024-10-08 13:42:34 +0900

    🐛 自己署名証明書に正しいドメイン名を指定

--- src/lib/cert.ts
+++ src/lib/cert.ts
@@ -3,10 +3,10 @@ import fs from 'fs-extra';
 import path from 'path';
 
 export const generateCert = async (outDir: string) => {
-  const { stdout } = await exec(`mkcert localhost`);
+  const { stdout } = await exec(`mkcert localhost 127.0.0.1 ::1`);
   [
-    { input: 'localhost.pem', output: 'localhost-cert.pem' },
-    { input: 'localhost-key.pem', output: 'localhost-key.pem' },
+    { input: 'localhost+2.pem', output: 'localhost-cert.pem' },
+    { input: 'localhost+2-key.pem', output: 'localhost-key.pem' },
   ].forEach(({ input, output }) => {
     fs.moveSync(`./${input}`, path.join(outDir, output), {
       overwrite: true,
