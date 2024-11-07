6b6aa60e5f6e2aced71856323e9e526d56a94925
Ribbit <echo07200503@gmail.com>
2024-11-07 11:48:24 +0900

    🔥 vite

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import build from './commands/build.js';
-import viteBuild from './commands/build-vite.js';
 import dev from './commands/dev/index.js';
-import viteDev from './commands/dev-vite.js';
 import genkey from './commands/genkey.js';
 import esbuildBuild from './commands/build-esbuild.js';
 import lint from './commands/lint.js';
@@ -11,10 +9,8 @@ import lint from './commands/lint.js';
 program.name('k2').version('1.4.0').description('k2 - 🍳 kintone kitchen 🍳');
 
 build();
-viteBuild();
 esbuildBuild();
 dev();
-viteDev();
 genkey();
 lint();
 
