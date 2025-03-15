14e22d77b0b670ec9474493639986e586f0aba4f
Ribbit <echo07200503@gmail.com>
2025-03-15 12:35:28 +0900

    ♻ pluginConditionsAtomの定義を修正し、focusAtomからatomに変更

--- templates/plugin/full/src/desktop/public-state.ts
+++ templates/plugin/full/src/desktop/public-state.ts
@@ -1,9 +1,8 @@
 import { isUsagePluginConditionMet, restorePluginConfig } from '@/lib/plugin';
 import { atom } from 'jotai';
-import { focusAtom } from 'jotai-optics';
 
 export const pluginConfigAtom = atom(restorePluginConfig());
-export const pluginConditionsAtom = focusAtom(pluginConfigAtom, (s) => s.prop('conditions'));
+export const pluginConditionsAtom = atom((get) => get(pluginConfigAtom).conditions);
 export const validPluginConditionsAtom = atom((get) =>
   get(pluginConditionsAtom).filter(isUsagePluginConditionMet)
 );
