7068fa7dabfbdb42324d3a34ef915d4714ca5521
Ribbit <echo07200503@gmail.com>
2025-03-22 11:13:28 +0900

    ♻ グローバル変数の定義を更新し、開発環境のチェックを追加

--- templates/plugin/full/src/lib/global.ts
+++ templates/plugin/full/src/lib/global.ts
@@ -2,14 +2,16 @@ import { detectGuestSpaceId } from '@konomi-app/kintone-utilities';
 
 export const ENV = (process?.env?.NODE_ENV ?? 'production') as 'production' | 'development';
 export const isProd = ENV === 'production';
+export const isDev = ENV === 'development';
 
 export const PLUGIN_ID = kintone.$PLUGIN_ID;
 export const GUEST_SPACE_ID = detectGuestSpaceId() ?? undefined;
 export const LANGUAGE = kintone.getLoginUser()?.language;
 
-!isProd &&
+if (!isProd) {
   console.log('[plugin] Global variables have been redefined', {
     PLUGIN_ID,
     GUEST_SPACE_ID,
     LANGUAGE,
   });
+}
