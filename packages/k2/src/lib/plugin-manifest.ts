72365f0136753db5c4d6f527b57208e09d040eeb
adachi <echo07200503@gmail.com>
2025-09-28 08:50:49 +0900

    🔧 deepmergeをremedaのmergeDeepに置き換え、マニフェストのマージ処理を改善

--- packages/k2/src/lib/plugin-manifest.ts
+++ packages/k2/src/lib/plugin-manifest.ts
@@ -2,7 +2,7 @@ import { PLUGIN_CONTENTS_DIRECTORY } from './constants.js';
 import { importK2PluginConfig } from './import.js';
 import fs from 'fs-extra';
 import path from 'path';
-import merge from 'deepmerge';
+import { mergeDeep } from 'remeda';
 
 export const outputManifest = async (
   env: 'dev' | 'prod' | 'standalone',
@@ -10,7 +10,7 @@ export const outputManifest = async (
 ): Promise<Plugin.Meta.Manifest> => {
   const config = options?.config || (await importK2PluginConfig());
 
-  const merged = merge(config.manifest.base, config.manifest[env] || {}) as Plugin.Meta.Manifest;
+  const merged = mergeDeep(config.manifest.base, config.manifest[env] || {}) as Plugin.Meta.Manifest;
 
   await fs.mkdirs(PLUGIN_CONTENTS_DIRECTORY);
   await fs.writeJson(path.join(PLUGIN_CONTENTS_DIRECTORY, 'manifest.json'), merged);
