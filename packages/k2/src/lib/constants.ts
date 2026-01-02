23f23d0b7a6e9ba230356e1cd49ff90803d61e23
adachi <echo07200503@gmail.com>
2026-01-02 17:08:17 +0900

    ✨ プロダクションディレクトリの定義を追加

--- packages/k2/src/lib/constants.ts
+++ packages/k2/src/lib/constants.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 export const WORKSPACE_DIRECTORY = '.k2';
 export const DEVELOPMENT_DIRECTORY = path.join(WORKSPACE_DIRECTORY, 'dev');
+export const PRODUCTION_DIRECTORY = path.join(WORKSPACE_DIRECTORY, 'prod');
 
 export const CONFIG_FILE_NAME = 'k2.config.mjs';
 export const PLUGIN_CONFIG_FILE_NAME = 'plugin.config.mjs';
@@ -12,6 +13,8 @@ export const PLUGIN_WORKSPACE_DIRECTORY = '.plugin';
 export const PLUGIN_CONTENTS_DIRECTORY = path.join(PLUGIN_WORKSPACE_DIRECTORY, 'contents');
 /** kintoneプラグインの開発時、ローカルサーバーに反映するファイルを管理するフォルダ */
 export const PLUGIN_DEVELOPMENT_DIRECTORY = path.join(PLUGIN_WORKSPACE_DIRECTORY, 'dev');
+/** kintoneプラグインの本番ビルド出力ディレクトリ */
+export const PLUGIN_PRODUCTION_DIRECTORY = path.join(PLUGIN_WORKSPACE_DIRECTORY, 'prod');
 
 /** ローカルサーバーのデフォルトポート番号 */
 export const DEFAULT_PORT = 32767;
