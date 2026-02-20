ca955c62619605a4d6919ec835b4bea70ce85bcd
adachi <adachi@konomi.app>
2026-02-21 08:29:43 +0900

    ♻ configインポートパスを修正

--- templates/plugin/turbo/src/desktop/event.tsx
+++ templates/plugin/turbo/src/desktop/event.tsx
@@ -1,6 +1,6 @@
 import { manager } from '@/lib/event-manager';
 import { restorePluginConfig } from '@/lib/plugin';
-// import config from 'plugin.config.mjs';
+// import config from '@/../plugin.config.mjs';
 
 // const ROOT_ID = `🐸${config.id}-root`;
 
